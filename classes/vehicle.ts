af7f7e4a50918de38cfbf12abc6ea617ba3c762b
Tom Low <thomaslsternberg@gmail.com>
2021-05-07 22:21:18 -0400

    inheritance and modifiers

---
+++ classes/vehicle.ts
@@ -0,0 +1,23 @@
+class Vehicle {
+  protected drive(): void {
+    console.log('Vroom vroom chugga choo');
+  }
+}
+
+const vehicle = new Vehicle();
+
+class Car extends Vehicle {
+  // private drive(): void {
+  //   console.log('Just car sounds');
+  // }
+
+  startDrivingProces(): void {
+    this.drive(); //works bc refs other class mthd. If override method, cannot change modifier. Why is it overriding the method specifically that makes it angry about modifiers? If you can override the method, why not override the modifiers? Because of situations like this: https://stackoverflow.com/questions/6236909/why-cant-we-change-access-modifier-while-overriding-methods-in-c
+
+    //Use protected to allow classes to be called within their own classes and in child classes, but not outside.
+  }
+}
+
+const car = new Car();
+
+car.drive(); //doesn't work, bc private

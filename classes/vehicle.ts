b1024334b0771c727cb060e804164dc7a1c778ea
Tom Low <thomaslsternberg@gmail.com>
2021-05-13 15:59:49 -0400

    edits

--- classes/vehicle.ts
+++ classes/vehicle.ts
@@ -11,7 +11,7 @@ class Car extends Vehicle {
   //   console.log('Just car sounds');
   // }
 
-  startDrivingProces(): void {
+  startDrivingProcess(): void {
     this.drive(); //works bc refs other class mthd. If override method, cannot change modifier. Why is it overriding the method specifically that makes it angry about modifiers? If you can override the method, why not override the modifiers? Because of situations like this: https://stackoverflow.com/questions/6236909/why-cant-we-change-access-modifier-while-overriding-methods-in-c
 
     //Use protected to allow classes to be called within their own classes and in child classes, but not outside.
